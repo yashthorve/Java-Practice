7301542df686eded9b331d81fdf2f10701542487
Yash Thorve <yashthorve96@gmail.com>
2026-07-18 23:02:04 +0530

    Dec to Binary

--- BintoDec.java
+++ BintoDec.java
@@ -2,21 +2,34 @@
 import java.util.*;
 class BintoDec{
 
-public static int BintoDec(int bin){
+// public static int BintoDec(int bin){
+//     int pow=0;
+//     int dec=0;
+//     while(bin>0){
+//         int lastdig=bin%10;
+//         dec=dec+(lastdig*(int)Math.pow(2,pow));
+//         bin=bin/10;
+//         pow++;
+
+//     }
+//     return dec;
+// }
+public static int DectoBin(int dec){
     int pow=0;
-    int dec=0;
-    while(bin>0){
-        int lastdig=bin%10;
-        dec=dec+(lastdig*(int)Math.pow(2,pow));
-        bin=bin/10;
+    int bin=0;
+    while(dec>0){
+        int rem=dec%2;
+        bin=bin+(rem*(int)Math.pow(10,pow));
+        dec=dec/2;
         pow++;
 
     }
-    return dec;
+    return bin;
 }
     public static void main(String args[]){
-        int n = 101;
-        System.out.println(BintoDec(n));
+        // int n = 101;
+        int p=11;
+        System.out.println(DectoBin(p));
         }
 
 
