428b31ed2382cc61f92273f2d1cc7a4811cc10b0
Yash Thorve <yashthorve96@gmail.com>
2026-07-20 16:07:11 +0530

    #53 Best

---
+++ Kadane.java
@@ -0,0 +1,29 @@
+ class Kadane {
+    public static int maxSubArray(int[] nums) {
+            int currSum=nums[0];
+            int max=nums[0];
+           
+            for(int i=1;i<nums.length;i++){
+                currSum=Math.max(nums[i],currSum+nums[i]);
+                max=Math.max(max,currSum);
+
+              
+        }
+
+            return max;
+
+    }
+    
+
+
+public static void main(String[] args) {
+      
+        int arr[]={-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(arr));
+    
+
+}
+
+
+ }
+   
